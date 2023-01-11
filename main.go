68c6a2229d1e18fecf9a9f37477d08b428285a30
Hudayi <asikhudayi@gmail.com>
2023-01-12 00:40:58 +0300

    snake adaptation. only grows size 2. will fixed

--- main.go
+++ main.go
@@ -2,51 +2,126 @@ package main
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
+	"math/rand"
 	"os"
 )
 
+type snake struct {
+	x, y int
+}
+
 type game struct {
-	x, y    int
-	height  int
-	width   int
-	running bool
+	snake     *list.List
+	food      *food
+	height    int
+	width     int
+	running   bool
+	direction int // 0-up, 1-down, 2-left, 3-right
+
+}
+type food struct {
+	x, y int
+}
+
+func (g *game) init_food() {
+	// Initialize the food struct with random x and y positions within the grid
+	g.food = &food{rand.Intn(g.height), rand.Intn(g.width)}
+}
+
+func (g *game) check_food() {
+	// check if snake's head is on the food
+	if g.snake.Front().Value.(*snake).x == g.food.x && g.snake.Front().Value.(*snake).y == g.food.y {
+		// Add a new element to the snake's body
+		g.snake.PushBack(&snake{g.snake.Back().Value.(*snake).x, g.snake.Back().Value.(*snake).y})
+		// Generate a new food at a random position
+		g.init_food()
+	}
 }
 
 var key string // keyboard input (w, s, a, d, q)
 
-func (g *game) update(key rune) {
+func (g *game) set_direction(key rune) {
 	if key == ' ' {
 		return
 	}
-
 	switch key {
 	case 'w': //up
-		if g.x > 0 {
-			g.x--
+		if g.direction != 1 {
+			g.direction = 0
 		}
+
 	case 's': //down
-		if g.x < g.height-1 {
-			g.x++
+		if g.direction != 0 {
+			g.direction = 1
 		}
+
 	case 'a': //left
-		if g.y > 0 {
-			g.y--
+		if g.direction != 3 {
+			g.direction = 2
 		}
 	case 'd': //right
-		if g.y < g.width-1 {
-			g.y++
+		if g.direction != 2 {
+			g.direction = 3
 		}
 	case 'q': //quit
 		g.running = false
 	}
+
 }
 
-func (g *game) display() { // display the map
+func (g *game) move_tails() {
+	// Iterate through the elements in the list, starting from the second element
+	for e := g.snake.Front().Next(); e != nil; e = e.Next() {
+		// Set the current element's x and y to the x and y of the previous element
+		e.Value.(*snake).x = e.Prev().Value.(*snake).x
+		e.Value.(*snake).y = e.Prev().Value.(*snake).y
+	}
+}
+
+func (g *game) update() {
+
+	dir := g.direction
+
+	g.move_tails()
+	switch dir {
+	case 0: //up
+		// game.snake.Front
+		if g.snake.Front().Value.(*snake).x > 0 {
+			g.snake.Front().Value.(*snake).x -= 1
+		}
+	case 1: //down
+		if g.snake.Front().Value.(*snake).x < g.height-1 {
+			g.snake.Front().Value.(*snake).x += 1
+		}
+	case 2: //left
+		if g.snake.Front().Value.(*snake).y > 0 {
+			g.snake.Front().Value.(*snake).y -= 1
+		}
+
+	case 3: //right
+		if g.snake.Front().Value.(*snake).y < g.width-1 {
+			g.snake.Front().Value.(*snake).y += 1
+		}
+	}
+
+}
+func elementInList(l *list.List, x int, y int) bool {
+	for e := l.Front(); e != nil; e = e.Next() {
+		if e.Value.(*snake).x == x && e.Value.(*snake).y == y {
+			return true
+		}
+	}
+	return false
+}
+func (g *game) display() {
 	for i := 0; i < g.height; i++ {
 		for j := 0; j < g.width; j++ {
-			if i == g.x && j == g.y {
-				fmt.Print("O") // player
+			if elementInList(g.snake, i, j) {
+				fmt.Print("O") // snake
+			} else if i == g.food.x && j == g.food.y {
+				fmt.Print("X") // food
 			} else {
 				fmt.Print(".") // empty
 			}
@@ -54,6 +129,7 @@ func (g *game) display() { // display the map
 		fmt.Println() // new line
 	}
 }
+
 func read_input() rune {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -66,10 +142,17 @@ func read_input() rune {
 }
 
 func main() {
-	g := game{0, 0, 10, 20, true} // x,y,height,width,running
+	s := snake{0, 3}
+	f := food{0, 0}
+	g := game{list.New(), &f, 10, 10, true, 2}
+	g.snake.PushBack(&s) // head
+	g.init_food()
+
 	for g.running {
 		g.display()
 		key := read_input()
-		g.update(key)
+		g.set_direction(key)
+		g.check_food()
+		g.update()
 	}
 }
